9bb03ddad1e90e9515cd0c33e7c3cca406105d15
Manohar <2431043@cognizant.com>
2025-08-25 11:33:41 +0530

    fit

---
+++ Food-delivery/Data/FoodDeliveryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Food_delivery.Models; // Assuming FoodPost is in this namespace
+using Food_delivery.Models.Domain;
+using Microsoft.EntityFrameworkCore; // Required for DbContext and DbSet
+
+namespace Food_delivery.Data
+{
+    public class FoodDeliveryDbContext : DbContext
+    {
+        public FoodDeliveryDbContext(DbContextOptions<FoodDeliveryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodPost> FoodPosts { get; set; } = null!;
+    }
+}
